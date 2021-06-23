3a6d0ef8bbc82612a294ddc9443568bdc790eecc
Dhruv Intwala <dhruv.intwala@bacancy.com>
2021-06-23 18:52:03 +0530

    routing set for api

---
+++ app/controllers/api/v1/images_controller.rb
@@ -0,0 +1,6 @@
+module Api
+  module V1
+    class ImagesController < ApplicationController
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :images
+    end
+  end
+
+  get '*path', to: 'home#index', via: :all
 end
