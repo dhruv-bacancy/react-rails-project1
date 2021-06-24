f44e576355f3cf5e32b620bf33aa9723e4a1787d
Dhruv Intwala <dhruv.intwala@bacancy.com>
2021-06-24 12:04:35 +0530

    images controller set

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,6 +1,53 @@
 module Api
   module V1
     class ImagesController < ApplicationController
+
+      def index
+        images = Image.all
+        
+        render json: ImageSerializer.new(images).serialized_json
+      end
+
+      def show
+        image = Image.find(params[:id])
+        
+        render json: ImageSerializer.new(image).serialized_json
+      end
+
+      def create
+        image = Image.create(image_params)
+
+        if image.save
+          render json: ImageSerializer.new(image).serialized_json
+        else
+          render json: { error: image.errors.messages }, status: 422
+        end
+      end
+
+      def update
+        image = Image.find(params[:id])
+
+        if image.update(image_params)
+          render json: ImageSerializer.new(image).serialized_json
+        else
+          render json: { error: image.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        image = Image.find(params[:id])
+
+        if image.destroy
+          head :no_content
+        else
+          render json: { error: image.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def image_params
+        params.require(:image).permit(:name, :image_url, :description)
     end
   end
 end
