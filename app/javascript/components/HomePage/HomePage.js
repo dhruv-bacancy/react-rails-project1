fd36a68027b49072e20c043b4addb0400a065940
Dhruv Intwala <dhruv.intwala@bacancy.com>
2021-06-24 21:55:56 +0530

    delete requests

--- app/javascript/components/HomePage/HomePage.js
+++ app/javascript/components/HomePage/HomePage.js
@@ -6,16 +6,25 @@ function HomePage() {
   const [imageData, setImageData] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/v1/images.json")
-    .then(response => {
+    axios.get("http://localhost:3000/api/v1/images.json").then((response) => {
       setImageData(response.data.data);
     });
   }, []);
 
+  const deleteHandler = (id) => {
+    setImageData(imageData.filter((img) => img.id != id));
+    const csrfToken = document.querySelector("[name=csrf-token]").content;
+    axios.defaults.headers.common["X-CSRF-TOKEN"] = csrfToken;
+    axios
+      .delete(`http://localhost:3000/api/v1/images/${id}`)
+      .then((resp) => console.log(resp))
+      .catch((resp) => console.log(resp));
+  };
+
   return (
     <div className="py-4">
       <h1 className="text-center">Home Page</h1>
-      <Table imagesList={imageData} />
+      <Table imagesList={imageData} deleteHandler={deleteHandler} />
     </div>
   );
 }
