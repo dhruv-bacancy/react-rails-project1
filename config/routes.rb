0b042a3d5608ad05fcaced135cf05fae438279ec
dhruv-bacancy <dhruv.intwala@bacancy.com>
2021-06-29 16:38:18 +0530

    controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :images
+      resources :sessions, only: [:create]
+      resources :registrations, only: [:create]
+      delete :logout, to: 'sessions#logout'
+      get :logged_in, to: 'sessions#logged_in'
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_121400) do
+ActiveRecord::Schema.define(version: 2021_06_29_081431) do
 
   create_table "images", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
